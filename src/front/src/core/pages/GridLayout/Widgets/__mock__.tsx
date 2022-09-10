63133de0c393a6cb7fedf6e666e3c8489e3dda9a
Ilya Fedorov <ilya-fedorov-2000@list.ru>
2022-09-10 14:47:34 +0500

    add: column chart

---
+++ src/front/src/core/pages/GridLayout/Widgets/__mock__.tsx
@@ -0,0 +1,7 @@
+export const MOCK_DATA_FOR_COLUMN_CHART = [
+    { parameter: 'Параметр 1', number: 1234 },
+    { parameter: 'Параметр 2', number: 1083 },
+    { parameter: 'Параметр 3', number: 672 },
+    { parameter: 'Параметр 4', number: 301 },
+    { parameter: 'Параметр 5', number: 167 },
+]

ea3bceec1edf7de9f2907f99d52ac1eb74e0e497
n19z <sharifyanov.niyaz@gmail.com>
2022-09-11 17:58:30 +0500

    feat(Contracts.tsx): Добавлена возможность скачивания файла
    - Обновлена кнопка скачивания
    - Добавлен тестовый pdf файл ./assets/__TestPdf__.pdf

--- src/front/src/core/pages/Contracts/Contracts.tsx
+++ src/front/src/core/pages/Contracts/Contracts.tsx
@@ -3,11 +3,12 @@ import css from '../Main.module.css'
 import { Table, TableColumn } from '@consta/uikit/Table';
 import { IconDownload } from '@consta/uikit/IconDownload';
 import { mockRowDataForTable } from './__mock__'
+import { Button } from '@consta/uikit/Button';
 interface Props {
 
 }
 const rows = mockRowDataForTable;
-
+let rowId: string | undefined;
 const columns: TableColumn<typeof rows[number]>[] = [
     {
         title: 'Номер договора',
@@ -33,15 +34,30 @@ const columns: TableColumn<typeof rows[number]>[] = [
     {
         title: '',
         accessor: 'download',
-        width: 80,
         align: 'center',
-        renderCell: () => <IconDownload view='link' size='s'/>
+        renderCell: (row: { id: string; }) => {
+            return <a href={require("../../../assets/__TestPdf__.pdf")} download={`__${row.id}__TestFile`}>
+                <Button
+                    size="s"
+                    label="Cкачать"
+                    title='Скачать договор'
+                    iconRight={IconDownload}
+                />
+            </a>
+        }
     },
 ];
+
 export const Contracts: React.FC<Props> = ({ }) => {
+
     return (
         <div className={css.container}>
-            <Table rows={rows} columns={columns} borderBetweenColumns zebraStriped="even"/>;
+            <Table
+                rows={rows}
+                columns={columns}
+                borderBetweenColumns
+                zebraStriped="even"
+            />;
         </div>
     );
 };

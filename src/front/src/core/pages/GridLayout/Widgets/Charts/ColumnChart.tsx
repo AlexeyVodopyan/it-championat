71245ff5d183078bdd06e3c6b045c4ab6b2e76e6
Ilya Fedorov <ilya-fedorov-2000@list.ru>
2022-09-11 12:53:31 +0500

    add: AreaChart

--- src/front/src/core/pages/GridLayout/Widgets/Charts/ColumnChart.tsx
+++ src/front/src/core/pages/GridLayout/Widgets/Charts/ColumnChart.tsx
@@ -8,8 +8,7 @@ interface IColumnChartProps {
 }
 
 export const ColumnChart: React.FC<IColumnChartProps> = props => {
-    return <Column
-            className={css.chartWidget}
+    return <Column className={css.chartWidget}
             data={MOCK_DATA_FOR_COLUMN_CHART}
             xField="parameter"
             yField="number"
